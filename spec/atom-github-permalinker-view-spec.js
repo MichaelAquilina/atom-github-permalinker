29da4ca6b33c538d336b3dda4210b11d85776c5b
Michael Aquilina <michaelaquilina@gmail.com>
2016-10-14 16:01:31 +0100

    Initial Commit

---
+++ spec/atom-github-permalinker-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomGithubPermalinkerView from '../lib/atom-github-permalinker-view';
+
+describe('AtomGithubPermalinkerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
