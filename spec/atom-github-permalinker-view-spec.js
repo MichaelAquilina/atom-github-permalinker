fcf22b740eb72f90f1a8ab8cea5ac63b6e18e738
Michael Aquilina <michaelaquilina@gmail.com>
2016-10-14 16:16:51 +0100

    First prototype for Github Permalinker

--- spec/atom-github-permalinker-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomGithubPermalinkerView from '../lib/atom-github-permalinker-view';
-
-describe('AtomGithubPermalinkerView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
